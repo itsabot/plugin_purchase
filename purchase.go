45509cbe4a97d4f8f3cdf820824cab801d183c39
Evan Tann <egtann@gmail.com>
2016-02-25 14:03:13 -0800

    Fix import paths

--- purchase.go
+++ purchase.go
@@ -11,13 +11,13 @@ import (
 	"time"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/itsabot/abot/shared/datatypes"
+	"github.com/itsabot/abot/shared/language"
+	"github.com/itsabot/abot/shared/nlp"
+	"github.com/itsabot/abot/shared/pkg"
+	"github.com/itsabot/abot/shared/task"
 	"github.com/jmoiron/sqlx"
 	"github.com/renstrom/fuzzysearch/fuzzy"
-	"itsabot.org/abot/shared/datatypes"
-	"itsabot.org/abot/shared/language"
-	"itsabot.org/abot/shared/nlp"
-	"itsabot.org/abot/shared/pkg"
-	"itsabot.org/abot/shared/task"
 )
 
 type Purchase string
