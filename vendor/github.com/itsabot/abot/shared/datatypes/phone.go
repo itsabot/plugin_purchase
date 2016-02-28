405ecdec2d4cd463c68c343b315926f49cef14a1
Evan Tann <egtann@gmail.com>
2016-02-27 17:47:28 -0800

    Remove vendored dependencies, use Glockfile

--- vendor/github.com/itsabot/abot/shared/datatypes/phone.go
+++
@@ -1,7 +0,0 @@
-package dt
-
-// Phone represents a phone as a flexid from the database.
-type Phone struct {
-	Id     uint64
-	Number string `db:"flexid"`
-}
--- vendor/github.com/itsabot/abot/shared/datatypes/time_range.go
+++
@@ -1,9 +0,0 @@
-package dt
-
-import "time"
-
-// TimeRange defines a range of time.
-type TimeRange struct {
-	Start *time.Time
-	End   *time.Time
-}
